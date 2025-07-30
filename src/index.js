b35a3e226acbbee0ce6347c460ac3f98f674673d
Dmitry Salashchenko <ho4houkot39@gmail.com>
2025-07-30 17:36:34 +0300

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = Math.abs(n).toString().split('').reverse().join('');
+  return parseInt(str, 10);
 };
